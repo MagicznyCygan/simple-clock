542fc33422602790e2af4794c4b2a0c04152ac7a
MagicznyCygan <81189090+MagicznyCygan@users.noreply.github.com>
2021-05-31 20:09:08 +0200

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+const hourContainer = document.getElementById('hour');
+const minuteContainer = document.getElementById('minute');
+const secondContainer = document.getElementById('second');
+
+
+
+
+const getTime = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    hourContainer.innerHTML = hour;
+    minuteContainer.innerHTML = minute;
+    secondContainer.innerHTML = second;
+
+}
+setInterval(getTime, 1000);
+getTime();
+
